2b953fd272a7111429d3fb9e26c575fa766227c5
Konstantin Makarchev <kostya27@gmail.com>
2016-02-15 22:38:44 +0300

    Benchmark for unpacker

---
+++ bench/unpack.rb
@@ -0,0 +1,24 @@
+require "msgpack"
+
+def test_unpack(name, count, data)
+  slice = data.to_msgpack
+  t = Time.now
+  print name
+  res = 0
+  count.times do |i|
+    obj = MessagePack.unpack(slice)
+    res += obj.size
+  end
+  puts " = #{res}, #{Time.now - t}"
+end
+
+t = Time.now
+
+test_unpack("small string", 1000000, "a" * 200)
+test_unpack("big string", 10000, "a" * 200000)
+test_unpack("hash string string", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = "value#{i}"; h })
+test_unpack("hash string float64", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = i / 10.0; h })
+test_unpack("array of strings", 10000, Array.new(1000) { |i| "data#{i}" })
+test_unpack("array of floats", 20000, Array.new(3000) { |i| i / 10.0 })
+
+puts "Summary time: #{Time.now - t}"
