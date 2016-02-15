7eefc63789533a5cdd06f2a5221731930e110c26
Konstantin Makarchev <kostya27@gmail.com>
2016-02-15 20:53:10 +0400

    better benchmarks

--- bench/pack.cr
+++ bench/pack.cr
@@ -1,65 +1,22 @@
 require "../src/msgpack"
 
-def test_pack(name, count, &block : Int32 -> Slice(UInt8))
+def test_pack(name, count, data)
   t = Time.now
   print name
   res = 0
   count.times do |i|
-    res += block.call(i).size
+    res += MessagePack::Packer.new.write(data).to_slice.size
   end
   puts " = #{res}, #{Time.now - t}"
 end
 
 t = Time.now
 
-# =============================================
-
-s = "a" * 200
-test_pack("small string", 1000000) do
-  MessagePack::Packer.new.write(s).to_slice
-end
-
-# =============================================
-
-s = "a" * 200000
-test_pack("big string", 10000) do
-  MessagePack::Packer.new.write(s).to_slice
-end
-
-# =============================================
-
-h = {} of String => String
-1000.times do |i|
-  h["key#{i}"] = "value#{i}"
-end
-
-test_pack("hash string string", 10000) do
-  MessagePack::Packer.new.write(h).to_slice
-end
-
-# =============================================
-
-h2 = {} of String => Float64
-1000.times do |i|
-  h2["key#{i}"] = i / 10.0.to_f64
-end
-
-test_pack("hash string float64", 10000) do
-  MessagePack::Packer.new.write(h2).to_slice
-end
-
-# =============================================
-
-arr = Array.new(1000) { |i| "data#{i}" }
-test_pack("array of strings", 10000) do
-  MessagePack::Packer.new.write(arr).to_slice
-end
-
-# =============================================
-
-arr = Array.new(3000) { |i| i / 10.0 }
-test_pack("array of floats", 20000) do
-  MessagePack::Packer.new.write(arr).to_slice
-end
+test_pack("small string", 1000000, "a" * 200)
+test_pack("big string", 10000, "a" * 200000)
+test_pack("hash string string", 10000, (0..1000).reduce({} of String => String) { |h, i| h["key#{i}"] = "value#{i}"; h })
+test_pack("hash string float64", 10000, (0..1000).reduce({} of String => Float64) { |h, i| h["key#{i}"] = i / 10.0.to_f64; h })
+test_pack("array of strings", 10000, Array.new(1000) { |i| "data#{i}" })
+test_pack("array of floats", 20000, Array.new(3000) { |i| i / 10.0 })
 
 puts "Summary time: #{Time.now - t}"
--- bench/pack.rb
+++ bench/pack.rb
@@ -1,66 +1,23 @@
 require "msgpack"
 # gem install msgpack
 
-def test_pack(name, count, &block)
+def test_pack(name, count, data)
   t = Time.now
   print name
   res = 0
   count.times do |i|
-    res += block.call(i).bytesize
+    res += data.to_msgpack.bytesize
   end
   puts " = #{res}, #{Time.now - t}"
 end
 
 t = Time.now
 
-# =============================================
-
-s = "a" * 200
-test_pack("small string", 1000000) do
-  s.to_msgpack
-end
-
-# =============================================
-
-s = "a" * 200000
-test_pack("big string", 10000) do
-  s.to_msgpack
-end
-
-# =============================================
-
-h = {}
-1000.times do |i|
-  h["key#{i}"] = "value#{i}"
-end
-
-test_pack("hash string string", 10000) do
-  h.to_msgpack
-end
-
-# =============================================
-
-h2 = {}
-1000.times do |i|
-  h2["key#{i}"] = i / 10.0
-end
-
-test_pack("hash string float64", 10000) do
-  h2.to_msgpack
-end
-
-# =============================================
-
-arr = Array.new(1000) { |i| "data#{i}" }
-test_pack("array of strings", 10000) do
-  arr.to_msgpack
-end
-
-# =============================================
-
-arr = Array.new(3000) { |i| i / 10.0 }
-test_pack("array of floats", 20000) do
-  arr.to_msgpack
-end
+test_pack("small string", 1000000, "a" * 200)
+test_pack("big string", 10000, "a" * 200000)
+test_pack("hash string string", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = "value#{i}"; h })
+test_pack("hash string float64", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = i / 10.0; h })
+test_pack("array of strings", 10000, Array.new(1000) { |i| "data#{i}" })
+test_pack("array of floats", 20000, Array.new(3000) { |i| i / 10.0 })
 
 puts "Summary time: #{Time.now - t}"
