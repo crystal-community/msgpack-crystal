fafa014358f684fd90ece63292c83160b15373ec
Konstantin Makarchev <kostya27@gmail.com>
2016-02-17 01:21:48 +0300

    fix

--- bench/unpack.rb
+++ bench/unpack.rb
@@ -21,7 +21,7 @@ test_unpack("hash string float64", 10000, (0..1000).reduce({}) { |h, i| h["key#{
 test_unpack("array of strings", 10000, Array.new(1000) { |i| "data#{i}" })
 test_unpack("array of floats", 20000, Array.new(3000) { |i| i / 10.0 })
 
-ints = [1, -1, 0x21, -0x21, 128, -128, -0x8000, 0x8000, 0xFFFF, -0xFFFF, -0x80000000, 0x80000000, -9223372036854775808, 9223372036854775808, 0xFFFFFFFF, -0xFFFFFFFF]
+ints = [1, -1, 0x21, -0x21, 128, -128, -0x8000, 0x8000, 0xFFFF, -0xFFFF, -0x80000000, 0x80000000, -9223372036854775808, 9223372036854775807, 4294967295, -4294967295]
 test_unpack("array of mix int sizes", 2000, Array.new(30000) { |i| ints[i % ints.size] })
 
 puts "Summary time: #{Time.now - t}"
