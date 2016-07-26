1a6a35fe8310089348bd3f144fb55bc758d5b9f2
Konstantin Makarchev <kostya27@gmail.com>
2016-07-26 20:11:27 +0400

    fix benchmarks

--- bench/run.sh
+++ bench/run.sh
@@ -1,19 +1,21 @@
-echo Crystal Generate File
+sh build.sh
+
+echo == Crystal Generate File
 ./bin_generate_file
-echo Crystal Load File
+echo == Crystal Load File
 ./bin_load_file
-echo Crystal Pack
+echo == Crystal Pack
 ./bin_pack
-echo Crystal Unpack
+echo == Crystal Unpack
 ./bin_unpack
 
-echo Ruby Generate File
+echo == Ruby Generate File
 ruby generate_file.rb
-echo Ruby Load File
+echo == Ruby Load File
 ruby load_file.rb
-echo Ruby Pack
+echo == Ruby Pack
 ruby pack.rb
-echo Ruby Unpack
+echo == Ruby Unpack
 ruby unpack.rb
 
 
--- bench/unpack.rb
+++ bench/unpack.rb
@@ -12,13 +12,25 @@ def test_unpack(name, count, data)
   puts " = #{res}, #{Time.now - t}"
 end
 
+def bytes(size)
+  "\0" * size
+end
+
+def byte(value)
+  [value].pack("C")
+end
+
 t = Time.now
 
 test_unpack("small string", 1000000, "a" * 200)
+test_unpack("small binary", 1000000, bytes(200))
 test_unpack("big string", 10000, "a" * 200000)
+test_unpack("big binary", 10000, bytes(200000))
 test_unpack("hash string string", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = "value#{i}"; h })
+test_unpack("hash string binary", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = byte(i); h })
 test_unpack("hash string float64", 10000, (0..1000).reduce({}) { |h, i| h["key#{i}"] = i / 10.0; h })
 test_unpack("array of strings", 10000, Array.new(1000) { |i| "data#{i}" })
+test_unpack("array of binaries", 10000, Array.new(1000) { |i| byte(i) })
 test_unpack("array of floats", 20000, Array.new(3000) { |i| i / 10.0 })
 
 ints = [1, -1, 0x21, -0x21, 128, -128, -0x8000, 0x8000, 0xFFFF, -0xFFFF, -0x80000000, 0x80000000, -9223372036854775808, 9223372036854775807, 4294967295, -4294967295]
