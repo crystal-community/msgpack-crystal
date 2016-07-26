f3f76807f784917b006a0e3814d3865ccd97ac21
Konstantin Makarchev <kostya27@gmail.com>
2016-07-26 20:00:35 +0400

    fix benchmarks

---
+++ bench/build.sh
@@ -0,0 +1,5 @@
+crystal build generate_file.cr --release -o bin_generate_file
+crystal build load_file.cr --release -o bin_load_file
+crystal build pack.cr --release -o bin_pack
+crystal build unpack.cr --release -o bin_unpack
+
---
+++ bench/run.sh
@@ -0,0 +1,19 @@
+echo Crystal Generate File
+./bin_generate_file
+echo Crystal Load File
+./bin_load_file
+echo Crystal Pack
+./bin_pack
+echo Crystal Unpack
+./bin_unpack
+
+echo Ruby Generate File
+ruby generate_file.rb
+echo Ruby Load File
+ruby load_file.rb
+echo Ruby Pack
+ruby pack.rb
+echo Ruby Unpack
+ruby unpack.rb
+
+
