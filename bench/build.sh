3e8c1240dbdbd61f4cd82c47bbb849e10a980b41
Konstantin Makarchev <kostya27@gmail.com>
2019-09-29 20:01:57 +0300

    fix build.sh in bench

--- bench/build.sh
+++ bench/build.sh
@@ -1,5 +1,5 @@
-crystal build generate_file_serializable.cr --release -o bin_generate_file_ser
-crystal build load_file_serializable.cr --release -o bin_load_file_ser
-crystal build pack.cr --release -o bin_pack
-crystal build unpack.cr --release -o bin_unpack
-crystal build pack_unpack.cr --release -o bin_pack_unpack
+crystal build generate_file_serializable.cr --release -o bin_generate_file_ser --no-debug
+crystal build load_file_serializable.cr --release -o bin_load_file_ser --no-debug
+crystal build pack.cr --release -o bin_pack --no-debug
+crystal build unpack.cr --release -o bin_unpack --no-debug
+crystal build pack_unpack.cr --release -o bin_pack_unpack --no-debug
