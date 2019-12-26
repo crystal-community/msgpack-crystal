50b414cf3966c32bf6b6edb3d96a48174c6747a2
Will Richardson <william.hamish@gmail.com>
2019-12-26 21:18:00 +1100

    Add MessagePack::Any wrapper
    Allows for quick access to arbitrary deserialized data, like YAML::Any
    and JSON::Any.

---
+++ spec/any_spec.cr
@@ -0,0 +1,157 @@
+require "./spec_helper"
+
+def as_any(obj)
+  MessagePack::Any.from_msgpack IO::Memory.new(obj.to_msgpack)
+end
+
+describe MessagePack::Any do
+  describe "casts" do
+    it "gets nil" do
+      as_any(nil).as_nil.should be_nil
+    end
+
+    it "gets bool" do
+      as_any(true).as_bool.should be_true
+      as_any(false).as_bool.should be_false
+      as_any(true).as_bool?.should be_true
+      as_any(false).as_bool?.should be_false
+      as_any(2).as_bool?.should be_nil
+    end
+
+    it "gets int32" do
+      as_any(123).as_i32.should eq(123)
+      as_any(123).as_i32?.should eq(123)
+      as_any(true).as_i32?.should be_nil
+    end
+
+    it "gets int64" do
+      as_any(123456789123456).as_i64.should eq(123456789123456)
+      as_any(123456789123456).as_i64?.should eq(123456789123456)
+      as_any(true).as_i64?.should be_nil
+    end
+
+    it "gets float32" do
+      as_any(123.45).as_f.should eq(123.45)
+      as_any(123.45).as_f?.should eq(123.45)
+      as_any(true).as_f?.should be_nil
+    end
+
+    it "gets float64" do
+      as_any(123.45).as_f.should eq(123.45)
+      as_any(123.45).as_f?.should eq(123.45)
+      as_any(true).as_f?.should be_nil
+    end
+
+    it "gets string" do
+      as_any("hello").as_s.should eq("hello")
+      as_any("hello").as_s?.should eq("hello")
+      as_any(true).as_s?.should be_nil
+    end
+
+    it "gets array" do
+      as_any([1, 2, 3]).as_a.should eq([1, 2, 3])
+      as_any([1, 2, 3]).as_a?.should eq([1, 2, 3])
+      as_any(true).as_a?.should be_nil
+    end
+
+    it "gets hash" do
+      as_any({"foo" => "bar"}).as_h.should eq({"foo" => "bar"})
+      as_any({"foo" => "bar"}).as_h?.should eq({"foo" => "bar"})
+      as_any(true).as_h?.should be_nil
+    end
+  end
+
+  describe "#size" do
+    it "of array" do
+      as_any([1, 2, 3]).size.should eq(3)
+    end
+
+    it "of hash" do
+      as_any({"foo" => "bar"}).size.should eq(1)
+    end
+  end
+
+  describe "#[]" do
+    it "of array" do
+      as_any([1, 2, 3])[1].raw.should eq(2)
+    end
+
+    it "of hash" do
+      as_any({"foo": "bar"})["foo"].raw.should eq("bar")
+    end
+  end
+
+  describe "#[]?" do
+    it "of array" do
+      as_any([1, 2, 3])[1]?.not_nil!.raw.should eq(2)
+      as_any([1, 2, 3])[3]?.should be_nil
+      as_any([true, false])[1]?.should eq false
+    end
+
+    it "of hash" do
+      as_any({"foo" => "bar"})["foo"]?.not_nil!.raw.should eq("bar")
+      as_any({"foo" => "bar"})["fox"]?.should be_nil
+      as_any({"foo" => false})["foo"]?.should eq false
+    end
+  end
+
+  describe "#dig?" do
+    it "gets the value at given path given splat" do
+      obj = as_any({"foo" => [1, {"bar" => [2, 3]}]})
+
+      obj.dig?("foo", 0).should eq(1)
+      obj.dig?("foo", 1, "bar", 1).should eq(3)
+    end
+
+    it "returns nil if not found" do
+      obj = as_any({"foo": [1, {"bar": [2, 3]}]})
+
+      obj.dig?("foo", 10).should be_nil
+      obj.dig?("bar", "baz").should be_nil
+      obj.dig?("").should be_nil
+    end
+  end
+
+  describe "dig" do
+    it "gets the value at given path given splat" do
+      obj = as_any({"foo": [1, {"bar": [2, 3]}]})
+
+      obj.dig("foo", 0).should eq(1)
+      obj.dig("foo", 1, "bar", 1).should eq(3)
+    end
+
+    it "raises if not found" do
+      obj = as_any({"foo" => [1, {"bar" => [2, 3]}]})
+
+      expect_raises Exception, %(Expected Array or Hash for #[], not Array(MessagePack::Type)) do
+        obj.dig("foo", 1, "bar", "baz")
+      end
+      expect_raises KeyError, %(Missing hash key: "z") do
+        obj.dig("z")
+      end
+      expect_raises KeyError, %(Missing hash key: "") do
+        obj.dig("")
+      end
+    end
+  end
+
+  it "traverses big structure" do
+    obj = as_any({"foo" => [1, {"bar": [2, 3]}]})
+    obj["foo"][1]["bar"][1].as_i32.should eq(3)
+  end
+
+  it "compares to other objects" do
+    obj = as_any([1, 2])
+    obj.should eq([1, 2])
+    obj[0].should eq(1)
+  end
+
+  it "can compare with ===" do
+    (1 === as_any(1)).should be_truthy
+  end
+
+  it "exposes $~ when doing Regex#===" do
+    (/o+/ === as_any("foo")).should be_truthy
+    $~[0].should eq("oo")
+  end
+end
---
+++ src/message_pack/any.cr
@@ -0,0 +1,180 @@
+module MessagePack
+  # A wrapper around a MessagePack object with convenience methods for type conversions
+  struct Any
+    getter raw : Type
+
+    def initialize(@raw)
+    end
+
+    def self.new(unpacker : IOUnpacker)
+      new(unpacker.read)
+    end
+
+    def self.from_msgpack(io)
+      new(MessagePack.unpack(io))
+    end
+
+    def dup
+      Any.new @raw.dup
+    end
+
+    def clone
+      Any.new @raw.clone
+    end
+
+    {% for name, type in {
+                           nil:  Nil,
+                           bool: Bool,
+                           f:    Float64,
+                           s:    String,
+                           h:    Hash(Type, Type),
+                           a:    Array(Type),
+                         } %}
+      # Convert to {{ type }}
+      def as_{{ name }} : {{ type }}
+        @raw.as {{ type }}
+      end
+
+      # Maybe convert to {{ type }}?
+      def as_{{ name }}? : {{ type }}?
+        @raw.as? {{ type }}
+      end
+    {% end %}
+
+    {% for size in {8, 16, 32, 64} %}
+      {% for sign, type_prefix in {i: "Int", u: "UInt"} %}
+        {% name = "#{sign}#{size}".id %}
+        {% type = "#{type_prefix.id}#{size}".id %}
+        # Convert to {{ type }}
+        def as_{{ name }} : {{ type }}
+          %raw = @raw
+          return {{ type }}.new(%raw) if %raw.responds_to? :to_{{ name }}
+          raise "Cannot cast #{@raw.class} to {{ type }}"
+        end
+
+        # Maybe convert to {{ type }}?
+        def as_{{ name }}? : {{ type }}?
+          %raw = @raw
+          return {{ type }}.new(%raw) if %raw.responds_to? :to_{{ name }}
+          nil
+        end
+      {% end %}
+    {% end %}
+
+    def [](key : Symbol) : Any
+      self[key.to_s.as(Type)]
+    end
+
+    def [](key : Type) : Any
+      if (index = key.as?(Int)) && (arr = @raw.as? Array(Type))
+        return Any.new(arr[index])
+      elsif hash = @raw.as? Hash(Type, Type)
+        return Any.new hash[key]
+      else
+        raise "Expected Array or Hash for #[], not #{@raw.class}"
+      end
+    end
+
+    def []?(key : Type) : Any?
+      if (index = key.as?(Int)) && (arr = @raw.as? Array(Type))
+        value = arr[index]?
+        unless value.nil?
+          return Any.new value
+        end
+      elsif hash = @raw.as? Hash(Type, Type)
+        value = hash[key]?
+        unless value.nil?
+          return Any.new value
+        end
+      else
+        raise "Expected Array or Hash for #[]?, not #{@raw.class}"
+      end
+    end
+
+    def each
+      if arr = @raw.as? Array(Type)
+        arr.each do |a|
+          yield Any.new(a)
+        end
+      elsif hash = @raw.as? Hash(Type, Type)
+        hash.each do |k, v|
+          yield Any.new(k), Any.new(v)
+        end
+      else
+        raise "Expected Array or Hash for #each, not #{@raw.class}"
+      end
+    end
+
+    def size
+      r = @raw
+      case r
+      when Hash(Type, Type), Array(Type)
+        return r.size
+      else
+        raise "Expected Array or Hash for #each, not #{@raw.class}"
+      end
+    end
+
+    def dig?(key : Type, *subkeys)
+      if (value = self[key]?) && value.responds_to?(:dig?)
+        value.dig?(*subkeys)
+      end
+    end
+
+    # :nodoc:
+    def dig?(key : Type)
+      self[key]?
+    end
+
+    def dig(key : Type, *subkeys)
+      if (value = self[key]) && value.responds_to?(:dig)
+        return value.dig(*subkeys)
+      end
+      raise "MessagePack::Any value not diggable for key: #{key.inspect}"
+    end
+
+    def dig(key : Type)
+      self[key]
+    end
+
+    delegate to_json, to_msgpack, to_s, inspect, :==, to: @raw
+  end
+end
+
+class Object
+  def ===(other : MessagePack::Any)
+    self === other.raw
+  end
+end
+
+struct Value
+  def ==(other : MessagePack::Any)
+    self == other.raw
+  end
+end
+
+class Reference
+  def ==(other : MessagePack::Any)
+    self == other.raw
+  end
+end
+
+class Array
+  def ==(other : MessagePack::Any)
+    self == other.raw
+  end
+end
+
+class Hash
+  def ==(other : MessagePack::Any)
+    self == other.raw
+  end
+end
+
+class Regex
+  def ===(other : MessagePack::Any)
+    value = self === other.raw
+    $~ = $~
+    value
+  end
+end
